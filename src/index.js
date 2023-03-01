bc96d3752ba75fa477d16a7effebf41f605dffcb
Arun Yadav <121511713+arunyadav-17@users.noreply.github.com>
2023-03-01 18:04:23 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Store from './store';
+import { Provider } from 'react-redux';
+import {BrowserRouter} from "react-router-dom";
+
+ReactDOM.render(
+<Provider store={Store}>
+<BrowserRouter>
+<App />
+</BrowserRouter>
+</Provider> 
+,document.getElementById('root'));
+
